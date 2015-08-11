ce134b507bd9f33ffc9fff67e3b9ad01f79cd541
Calandra Zellner <calandrazellner@gmail.com>
2015-08-11 19:08:20 -0400

    updated js, user prompt and ability to imput new value added to document

--- Etch.js
+++ Etch.js
@@ -1,3 +1,4 @@
+//creates 16X16 grid
 $(document).ready(function(){
 var $grid = $('.cube');
     for(var i = 0; i < 16; i++){
@@ -9,14 +10,38 @@ var $grid = $('.cube');
             }
 });
 
+// allows user to draw on sketch pad
 $(document).ready(function(){
 	$(".square").hover(function(){
 		$(this).css("background-color", "hotpink");
 	});
 });
 
+// clears the grid
 $(document).ready(function(){
     $("#button").click(function(){
         $(".square").css("background-color", "transparent");
     });
+});
+
+// creates new grid and allows for drawing
+$(document).ready(function(){
+    $("#button").click(function(){
+        $(".square").detach();
+        var newG = prompt("would you like a new grid? Enter number between 1-110");
+        
+         var $grid = $(".cube");
+     for(var i = 0; i < newG; i++){
+        var row = '<div>';
+        for(var j = 0; j < newG; j++)
+        row += '<div class="square">' + j + '</div>';
+        row += '</div>';
+        $grid.append(row);
+        console.log(newG);
+    }
+    $(".square").hover(function(){
+		$(this).css("background-color", "hotpink");
+	});
+    
+});
 });
